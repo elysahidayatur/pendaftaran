328fa07bd7e31a64bdf502ed167d414175c9cf01
elysahidayatur <124320545+elysahidayatur@users.noreply.github.com>
2023-02-03 11:21:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php include('koneksi.php');
+session_start();?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Informasi</title>
+	<link rel ="stylesheet" type = "text/css" href="style.css">
+</head>
+<body>
+<h2><center >Informasi</center></h2>
+<h3>Telah di buka pendaftaran Lomba 17 Agustus</h3>
+<h3> klik link di bawah ini untuk ke from pendaftaran</h3>
+<a href ="home.php">Pendaftaran Lomba 17 Agustus</a>
+</body>
+</html>
